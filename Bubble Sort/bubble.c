cb91fed44eea799781e1d1fbf166373405070879
SurimYuk <33217962+SurimYuk@users.noreply.github.com>
2019-03-23 20:06:56 +0900

    Add files via upload

---
+++ Bubble Sort/bubble.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int temp;
+	int arr[10] = { 1,10,5,8,7,6,4,3,2,9 };
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 9 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+	return 0;
+}
